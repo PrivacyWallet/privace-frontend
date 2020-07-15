5f40469db2af5a245f9472afbbcd7b6e889ba078
whitesad <whitesad_01@outlook.com>
2020-07-16 06:56:18 +0800

    之前写的表单的版本，但是之前是直接没跑工程直接翻译的表单。现在看来好像会错了意跑不起来，先上传，待改

--- src/components/Sheet.vue
+++ src/components/Sheet.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
-    我是表单
-    {{contracts}}
+    <!-- 我是表单 -->
+    <!-- {{contracts}} -->
+    <form method="post">
+      <input type="text" name="age">
+      <input type="text" name="gender">
+      <input type="text" name="job">
+      <input type="text" name="salary">    
+      <input type="text" name="hometown"> 
+      <input type="text" name="marital_status"> 
+      <input type="text" name="city_visited"> 
+    </form>
   </div>
 </template>
 
