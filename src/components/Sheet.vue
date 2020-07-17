97d92bbe03a161fbd5e4549bc4fca36831373117
whitesad <whitesad_01@outlook.com>
2020-07-17 22:50:21 +0800

    将sheet恢复原状

--- src/components/Sheet.vue
+++ src/components/Sheet.vue
@@ -1,16 +1,7 @@
 <template>
   <div>
-    <!-- 我是表单 -->
-    <!-- {{contracts}} -->
-    <form method="post">
-      <input type="text" name="age">
-      <input type="text" name="gender">
-      <input type="text" name="job">
-      <input type="text" name="salary">    
-      <input type="text" name="hometown"> 
-      <input type="text" name="marital_status"> 
-      <input type="text" name="city_visited"> 
-    </form>
+    我是表单
+    {{contracts}}
   </div>
 </template>
 
